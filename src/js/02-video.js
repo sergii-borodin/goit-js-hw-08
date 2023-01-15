214ec06fa856db00989d30e7b79329daa98d1b99
BoroDinS <sergey.borodin.lg@gmail.com>
2023-01-15 22:32:51 +0100

    fix task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,9 +6,22 @@ const iframe = document.querySelector('iframe');
 const iframePlayer = new Player(iframe);
 
 iframePlayer.on('timeupdate', throttle(currentTime => {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
+       
+localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
+    
+ },1000));
 
-    localStorage.getItem("videoplayer-current-time");
-    },1000));
+ iframePlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+ }).catch(function (error) {
+    //  console.log('error', error);
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
- iframePlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+        default:
+            // some other error occurred
+            break;
+    }
+});
